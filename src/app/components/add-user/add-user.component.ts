e91af3e8371eb801c3677b283e2aeaad0ac3f729
udara <udara@gl.block-stars.com>
2023-03-02 20:02:40 +0530

    slights changed added to user add html

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -1,3 +1,4 @@
+import { UserValidatorService } from './../../_services/user-validator.service';
 import { UserExerciserReq } from './../../models/user-exerciser-req';
 import { UserExercise } from './../../models/user-exercise';
 import { Exercise } from './../../models/exercise';
@@ -21,60 +22,80 @@ import {
 })
 export class AddUserComponent implements OnInit {
   user = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    email: new FormControl(),
-    password: new FormControl(),
-    address: new FormControl(),
-    phoneNo: new FormControl(),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(10),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(10),
+    ]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    phoneNo: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
     role: new FormControl(),
-    trainee: new FormControl(),
-    trainer: new FormControl(),
   });
   exerciseList!: Exercise[];
   showAlert = false;
   constructor(
-    private formBuilder: FormBuilder,
     private userService: UserService,
     private exerciseService: ExerciceService,
-    private userExerciseService: UserExerciseService
+    private userExerciseService: UserExerciseService,
+    public customeValidationService: UserValidatorService
   ) {}
   ngOnInit(): void {}
 
   saveUser() {
-    console.log(this.user.value);
-    const userObject = new User();
-    (userObject.username = this.user.value.username),
-      (userObject.email = this.user.value.email);
-    userObject.password = this.user.value.password;
-    (userObject.phoneNo = this.user.value.phoneNo),
-      (userObject.address = this.user.value.address),
-      (userObject.roles = [this.user.value.role]);
-    this.userService.createUser(userObject).subscribe((response: any) => {
-      console.log(response);
-      this.showAlert = true;
-      const createdUser = response;
-      if (this.user.value.role === 'trainee') {
-        this.exerciseService.getAllExercises().subscribe((response2: any) => {
-          this.exerciseList = response2;
+    if (this.user.valid) {
+      console.log(this.user.value);
+      const userObject = new User();
+      (userObject.username = this.user.value.username),
+        (userObject.email = this.user.value.email);
+      userObject.password = this.user.value.password;
+      (userObject.phoneNo = this.user.value.phoneNo),
+        (userObject.address = this.user.value.address),
+        (userObject.roles = [this.user.value.role]);
+      this.userService.createUser(userObject).subscribe((response: any) => {
+        console.log(response);
+        this.showAlert = true;
+        const createdUser = response;
+        if (this.user.value.role === 'trainee') {
+          this.exerciseService.getAllExercises().subscribe((response2: any) => {
+            this.exerciseList = response2;
 
-          let userExercise = new UserExerciserReq();
+            let userExercise = new UserExerciserReq();
 
-          userExercise.Assined_Date = null;
-          userExercise.Completed_Date = null;
-          userExercise.comment = '';
-          userExercise.status = 'Not Started';
-          userExercise.traineeId = createdUser.id;
-          this.exerciseList.forEach((exercise) => {
-            userExercise.exercise = exercise.id;
-            this.userExerciseService
-              .addUserExercise(userExercise)
-              .subscribe((reponse3: any) => {
-                console.log(reponse3);
-              });
+            userExercise.Assined_Date = null;
+            userExercise.Completed_Date = null;
+            userExercise.comment = '';
+            userExercise.status = 'Not Started';
+            userExercise.traineeId = createdUser.id;
+            this.exerciseList.forEach((exercise) => {
+              userExercise.exercise = exercise.id;
+              this.userExerciseService
+                .addUserExercise(userExercise)
+                .subscribe((reponse3: any) => {
+                  console.log(reponse3);
+                });
+            });
           });
-        });
-      }
-    });
+        }
+      });
+    } else {
+      this.customeValidationService.validateAllFormFields(this.user);
+    }
   }
 
   closeAlert() {
